4f4f4857bdda0ae15280738b27f27b670ca8f4a0
pushokov <kruchkov85@gmail.com>
2023-07-14 16:17:16 +0300

    pre release 2

--- client/src/features/Game/GameCard.tsx
+++ client/src/features/Game/GameCard.tsx
@@ -1,7 +1,7 @@
-import React, { useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import { Question, Topic } from './type/gameType';
 import { RootStateType } from '../../store';
-import { useState } from 'react';
 import Modal from '../Modal/Modal';
 
 type QuestionPropsType = {
@@ -10,16 +10,34 @@ type QuestionPropsType = {
 
 function GameCard({ question }: QuestionPropsType): JSX.Element {
   const [modalActive, setModalActive] = useState(false);
+
+  const [answerResult, setAnswerResult] = useState('');
+
   const [answer, setAnswer] = useState('');
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    if (answer === question.answer) {
+      setAnswerResult('Все верно. Молодец!');
+      fetch()
+    } else {
+      setAnswerResult(`Ответ не верный, верный ответ ${question.answer}`);
+    }
+  };
 
   return (
     <>
       <Modal active={modalActive} setActive={setModalActive}>
-        <form>
+        <form onSubmit={(e) => handleSubmit(e)}>
           <div>
-            <img src={`${question.questionName}.jpeg`} style={{width: '75vh'}} alt="question for a gamer" />
+            <img
+              src={`${question.questionName}.jpeg`}
+              style={{ width: '75vh' }}
+              alt="question for a gamer"
+            />
             <input type="text" defaultValue={answer} onChange={(e) => setAnswer(e.target.value)} />
           </div>
+          <div className="">{answerResult}</div>
           <button className="btn" type="submit">
             Ответить
           </button>
