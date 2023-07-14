cb02afec2969c718ba0935b735c1d6027c3e2166
Helekozl <helekozl@gmail.com>
2023-07-14 13:43:52 +0300

    ещё ауф

--- client/src/features/Navbar/Navbar.tsx
+++ client/src/features/Navbar/Navbar.tsx
@@ -1,7 +1,21 @@
 import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
+import styles from './Navbar.module.scss';
+import { RootStateType } from '../../store';
 
 function Navbar(): JSX.Element {
+  const { user } = useSelector((store: RootStateType) => store.userReducer);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleLogOut: React.MouseEventHandler<HTMLAnchorElement> = async (
+    e
+  ): Promise<void> => {
+    e.preventDefault();
+    await fetch('/api/auth/logout');
+    dispatch({ type: 'auth/logout' });
+    navigate('/');
+  };
   return (
     <nav>
       <ul>
@@ -13,6 +27,42 @@ function Navbar(): JSX.Element {
         <li>
           <Link to="/game">Игра</Link>
         </li>
+        {!user?.name ? (
+                <>
+                  <li>
+                    <NavLink
+                      className={styles.navbar__link}
+                      to="/auth/login"
+                    >
+                      Войти
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      className={styles.navbar__link}
+                      to="/auth/registration"
+                    >
+                      Регистрация
+                    </NavLink>
+                  </li>
+                </>
+              ) : (
+                <>
+                  <NavLink className={styles.navbar__link} to="/game">
+                    Game
+                  </NavLink>
+                  <li className={styles.navbar__link}>Hello, {user?.name}</li>
+                  <li>
+                    <NavLink
+                      onClick={handleLogOut}
+                      className={styles.navbar__logout}
+                      to="/"
+                    >
+                      Выйти
+                    </NavLink>
+                  </li>
+                </>
+              )}
       </ul>
     </nav>
   );
--- client/src/store.ts
+++ client/src/store.ts
@@ -3,14 +3,13 @@ import { legacy_createStore as createStore, combineReducers } from 'redux';
 import { topicReducer } from './features/Game/redux/reducer';
 import { userReducer } from './features/User/redux/userReducer';
 
-
 const rootReducer = combineReducers({
     topicReducer,
     userReducer,
   });
-  
+
   // создаём стор (наше хранилище данных)
   export const store = createStore(rootReducer, composeWithDevTools());
-  
+
   /* Какой тип будет у функции store.getState -> тот и будет типа RootStateType */
   export type RootStateType = ReturnType<typeof store.getState>;
